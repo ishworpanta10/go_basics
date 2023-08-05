ead8fea68f34100d0a517ca6d19e069779d401b0
ishworpanta10 <ishworpanta10@gmail.com>
2023-08-05 22:20:32 +0545

    embeded structs

--- structs/main.go
+++ structs/main.go
@@ -6,6 +6,13 @@ type person struct {
 	firstName string
 	lastName  string
 	age       int
+
+	detail contactInfo
+}
+
+type contactInfo struct {
+	email string
+	phone int
 }
 
 func main() {
@@ -15,9 +22,14 @@ func main() {
 	var alex person
 	alex.firstName = "Alex"
 	alex.age = 30
+	alex.detail.email = "alex@gmail.com"
 
 	fmt.Printf("%+v \n", alex)
-	newPerson := person{firstName: "Ishwor", lastName: "Panta", age: 28}
+	newPerson := person{
+		firstName: "Ishwor",
+		lastName:  "Panta",
+		age:       28,
+		detail:    contactInfo{email: "ishwor@gmail.com", phone: 9842345689}}
 
 	fmt.Println("Full User Detail", newPerson)
 	fmt.Println(newPerson.firstName, "is", newPerson.age, "years of old.")
