c350740e810784cd0cac728a76dd02c52980bb74
ishworpanta10 <ishworpanta10@gmail.com>
2023-08-05 22:29:53 +0545

    receiver function with structs

--- structs/main.go
+++ structs/main.go
@@ -24,16 +24,31 @@ func main() {
 	alex.age = 30
 	alex.detail.email = "alex@gmail.com"
 
-	fmt.Printf("%+v \n", alex)
+	// fmt.Printf("%+v \n", alex)
+	alex.print()
 	newPerson := person{
 		firstName: "Ishwor",
 		lastName:  "Panta",
 		age:       28,
 		detail:    contactInfo{email: "ishwor@gmail.com", phone: 9842345689}}
 
-	fmt.Println("Full User Detail", newPerson)
+	// fmt.Println("Full User Detail", newPerson)
+	newPerson.print()
 	fmt.Println(newPerson.firstName, "is", newPerson.age, "years of old.")
 
 	// updating user in structs
+	newPerson.updateFirstName("Messi")
+	// first name is not updated , lets go to pointer in  Go
+	newPerson.print()
 
+	// Pointer in GO
+
+}
+
+func (p person) updateFirstName(firstName string) {
+	p.firstName = firstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
