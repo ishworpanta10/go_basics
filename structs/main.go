e3a8e3348640b583088c4b68c7ed258b10ebb748
ishworpanta10 <ishworpanta10@gmail.com>
2023-08-06 18:05:10 +0545

    completed pointer with structs

--- structs/main.go
+++ structs/main.go
@@ -37,18 +37,31 @@ func main() {
 	fmt.Println(newPerson.firstName, "is", newPerson.age, "years of old.")
 
 	// updating user in structs
-	newPerson.updateFirstName("Messi")
+	// newPerson.updateFirstName("Messi")
 	// first name is not updated , lets go to pointer in  Go
-	newPerson.print()
+	// newPerson.print()
 
 	// Pointer in GO
 
+	// newUserPointer := &newPerson // no need to assign this as go can do itself
+	//if we have pointer type receiver function so it update that person
+
+	newPerson.updateFirstName("Lionel")
+
+	newPerson.print()
+
 }
 
-func (p person) updateFirstName(firstName string) {
-	p.firstName = firstName
+func (pointerToPerson *person) updateFirstName(firstName string) {
+	(*pointerToPerson).firstName = firstName
 }
 
 func (p person) print() {
 	fmt.Printf("%+v\n", p)
 }
+
+/// value type in go
+// int, float, string, bool, structs => use pointers to change these values : copies new values to new memory location
+
+/// Reference type in go
+// slices, maps, channels, pointers, functions => it just refrence to same memory loaction
